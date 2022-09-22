4a667694bf45e8e7109e91af054db82066dbc5c6
proll4588 <58320875+proll4588@users.noreply.github.com>
2022-09-22 12:07:00 +0700

    updateData

--- src/firebase.ts
+++ src/firebase.ts
@@ -7,13 +7,7 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyC8zyukQ6MZNuO6FEC_7U2CC5ItKG5fp_Y",
-    authDomain: "librarytest-c315c.firebaseapp.com",
-    projectId: "librarytest-c315c",
-    storageBucket: "librarytest-c315c.appspot.com",
-    messagingSenderId: "582440830308",
-    appId: "1:582440830308:web:19958c50c5a347622b6671",
-    measurementId: "G-KC8JB9BVED",
+    // FireBase proj data
 };
 
 // Initialize Firebase
